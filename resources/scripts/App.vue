dba955f31eb7837501bfa7115b2dd66e0c24ea35
Dries Hooghe <dries.hooghe@meditech-pharma.com>
2018-09-19 13:26:55 +0200

    Setup tailwind - WIP

---
+++ resources/scripts/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="bg-grey">
+    <h1>Johan in de huis!</h1>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
