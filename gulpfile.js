3b906d50dcd64e8b3c6a2b231329ce9001f1e5df
Dries Hooghe <dries.hooghe@gmail.com>
2018-09-21 17:12:11 +0200

    add npm scripts for development and production

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const atimport = require('postcss-import');
+const cssnano = require('cssnano');
 const webpack_stream = require('webpack-stream')
 const webpack = require('webpack');
 const webpack_config = require('./webpack.config.js');
@@ -12,7 +13,21 @@ const webpack_config = require('./webpack.config.js');
 // -------------- TASKS --------------
 
 // Compile css
-gulp.task('styles', () => {
+gulp.task('css', () => {
+
+    let tailwindcss = require('tailwindcss');
+
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            atimport(),
+            tailwindcss('src/styles/tailwind.config.js'),
+            require('autoprefixer')
+        ]))
+        .pipe(gulp.dest('./static'));
+});
+
+gulp.task('css:prod', () => {
 
     let tailwindcss = require('tailwindcss');
 
@@ -22,12 +37,37 @@ gulp.task('styles', () => {
             atimport(),
             tailwindcss('src/styles/tailwind.config.js'),
             require('autoprefixer'),
+            cssnano()
         ]))
         .pipe(gulp.dest('./static'));
 });
 
+gulp.task('css:watch', function () {
+    gulp.watch('src/styles/**/*.scss', ['css']);
+});
+
 // Compile js
-gulp.task('scripts', () => {
+gulp.task('js', () => {
     return webpack_stream(webpack_config, webpack)
         .pipe(gulp.dest(`static/`));
+});
+
+gulp.task('js:watch', function () {
+    gulp.watch('src/scripts/**/*.+(js|ts|vue)', ['js']);
+});
+
+// Development tasks
+gulp.task('build:dev', function () {
+    gulp.start('css', 'js');
+});
+
+gulp.task('build:watch', function () {
+    gulp.watch('src/scripts/**/*.+(js|ts|vue)', ['js']);
+    gulp.watch('src/styles/**/*.scss', ['css']);
+});
+
+// Production tasks
+
+gulp.task('build:prod', function () {
+    gulp.start('css:prod', 'js');
 });
