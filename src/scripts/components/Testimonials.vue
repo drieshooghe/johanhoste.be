39e9750d07520338595da2267659fe1468d31b83
Dries Hooghe <dries.hooghe@meditech-pharma.com>
2018-11-17 17:15:12 +0100

    disable lightbox for the time being

--- src/scripts/components/Testimonials.vue
+++ src/scripts/components/Testimonials.vue
@@ -15,16 +15,16 @@
         </div>
       </section>
 
-      <LightBox></LightBox>
+      <!-- <LightBox></LightBox> -->
 
     </div>
 </template>
 
 <script>
 import PageTitle from "./partials/PageTitle.vue";
-import LightBox from "./partials/LightBox.vue";
-import * as PhotoSwipe from "photoswipe";
-import * as PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default";
+// import LightBox from "./partials/LightBox.vue";
+// import * as PhotoSwipe from "photoswipe";
+// import * as PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default";
 export default {
   name: "testimonials",
   components: {
@@ -70,67 +70,68 @@ export default {
     /**
      * For each image of this testimonial fetch a hiresolution image and append it to the lightbox gallery
      */
-    let promises = [];
-    let imageCollections = [];
-    for (let testim of this.testimonials) {
-      let images = [];
-      for (let img of testim.getImageCollection().getImages()) {
-        promises.push(
-          fetch(
-            this.$store.state.api.host +
-              "/api/cockpit/image?token=" +
-              this.$store.state.api.token,
-            {
-              method: "post",
-              mode: "cors",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                src: img.path || img.id,
-                w: 1080, // width
-                q: 80 // quality
-              })
-            }
-          )
-            .then(res => res.text())
-            .then(res => {
-              document
-                .getElementById(img.id)
-                .setAttribute("data-hires-src", res);
-              images.push({
-                src: res,
-                w: 1080,
-                h: 768
-              });
-              return true;
-            })
-            .catch(error => console.error(error))
-        );
-      }
-      imageCollections.push(images);
-    }
+    // let promises = [];
+    // let imageCollections = [];
+    // for (let testim of this.testimonials) {
+    //   let images = [];
+    //   for (let img of testim.getImageCollection().getImages()) {
+    //     promises.push(
+    //       fetch(
+    //         this.$store.state.api.host +
+    //           "/api/cockpit/image?token=" +
+    //           this.$store.state.api.token,
+    //         {
+    //           method: "post",
+    //           mode: "cors",
+    //           headers: { "Content-Type": "application/json" },
+    //           body: JSON.stringify({
+    //             src: img.path || img.id,
+    //             w: 1080, // width
+    //             q: 80 // quality
+    //           })
+    //         }
+    //       )
+    //         .then(res => res.text())
+    //         .then(res => {
+    //           let img = document.getElementById(img.id);
+    //           if (img !== null) {
+    //             img.setAttribute("data-hires-src", res);
+    //           }
+    //           images.push({
+    //             src: res,
+    //             w: 1080,
+    //             h: 768
+    //           });
+    //           return true;
+    //         })
+    //         .catch(err => console.error(err))
+    //     );
+    //   }
+    //   imageCollections.push(images);
+    // }
 
     /**
      * Render lightbox
      */
-    Promise.all(promises).then(res => {
-      for (let i in imageCollections) {
-        let gallery = new PhotoSwipe(
-          document.getElementsByClassName("pswp")[0],
-          PhotoSwipeUI_Default,
-          imageCollections[i],
-          {
-            index: 0,
-            bgOpacity: 0.6
-          }
-        );
-        let triggers = document.querySelectorAll(".js-lightbox-trigger-" + i);
-        for (let t of triggers) {
-          t.addEventListener("click", e => {
-            // gallery.init();
-          });
-        }
-      }
-    });
+    // Promise.all(promises).then(res => {
+    //   for (let i in imageCollections) {
+    //     let gallery = new PhotoSwipe(
+    //       document.getElementsByClassName("pswp")[0],
+    //       PhotoSwipeUI_Default,
+    //       imageCollections[i],
+    //       {
+    //         index: 0,
+    //         bgOpacity: 0.6
+    //       }
+    //     );
+    //     let triggers = document.querySelectorAll(".js-lightbox-trigger-" + i);
+    //     for (let t of triggers) {
+    //       t.addEventListener("click", e => {
+    //         // gallery.init();
+    //       });
+    //     }
+    //   }
+    // });
   }
 };
 </script>
