92f3f2c013790c9e0e22d748a6c65d96639ff1b7
Dries Hooghe <dries@drieshooghe.com>
2020-02-21 14:58:29 +0100

    configure under construction message

--- components/app.tsx
+++ components/app.tsx
@@ -2,7 +2,7 @@ import '../style/style.css';
 import { Component } from 'react';
 import Head from 'next/head';
 import PageTitle from './page-title';
-import NavBar from './navbar';
+import Maintenance from './maintenance/maintenance';
 
 
 class App extends Component {
@@ -10,14 +10,15 @@ class App extends Component {
 	render() {
 		return <div>
 			<Head>
-				<title>Dries Hooghe</title>
+				<title>Johan Hoste</title>
 				<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"></link>
 				<link rel="stylesheet" href="/_next/static/css/styles.chunk.css"/>
 			</Head>
 			<div className='app'>
 				<article className="container flex flex-col w-full h-full justify-center items-center">
 					<PageTitle/>
-					<NavBar/>
+					<Maintenance/>
+					{/* <NavBar/> */}
 				</article>
 			</div>
 		</div>;
---
+++ components/maintenance/maintenance.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Maintenance extends Component {
+	render() {
+		return <section className="pb-8">
+			<h3 className="text-white text-center">De website wordt momenteel geupdate<br/>probeer het later nog eens</h3>
+		</section>
+	}
+}
+
+export default Maintenance;
