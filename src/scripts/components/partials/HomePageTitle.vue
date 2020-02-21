4e9dc050243c185cbdbe955105e5651d1f7833f6
Dries Hooghe <dries@drieshooghe.com>
2020-02-21 14:31:28 +0100

    tabula rasa

--- src/scripts/components/partials/HomePageTitle.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1 class="text-white font-light font-menu text-4xl lg:text-5xl text-center">{{$store.getters.getPageTitle(this.$parent.$options.name)}}</h1>
-</template>
--- src/scripts/components/partials/PageTitle.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1 class="mt-4 py-8">{{$store.getters.getPageTitle(this.$parent.$options.name)}}</h1>
-</template>
