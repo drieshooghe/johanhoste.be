f7069a144a1e6842d187308c2a618168b0b34cee
Dries Hooghe <dries@drieshooghe.com>
2020-02-21 19:24:36 +0100

    add logo svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import { NavBar } from '../components/navigational/navbar';
 import { Content } from '../content';
 import { HeaderTags } from '../components/meta/tags'
+import { Logo } from '../components/logo';
 
 export default function Index() {
   return (
     <div>
       <HeaderTags />
-      <div className="font-sans bg-white w-screen max-w-full">
+      <div className="font-sans bg-white w-screen max-w-full bg-congress-blue">
         <div className="flex flex-col h-screen bg-cover-home justify-center items-center">
-          <div className="flex flex-col min-h-screen">
-            <img src="public/img/header_logo.svg" className="w-24 md:w-48 lg:w-64 mb-8" />
+          <div className="flex flex-col min-h-screen justify-center">
+            <img src="/public/home-background.jpg" alt=""/>
+            <Logo/>
             <h1 className="text-white font-light font-menu text-4xl lg:text-5xl text-center">{Content.pages.home.title}</h1>
             <div className="text-white font-light font-menu text-1xl lg:text-2xl text-center pb-3">{Content.pages.home.body}</div>
             <NavBar />
