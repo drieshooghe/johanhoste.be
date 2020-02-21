cfbef329bef841773bc999f7e1967cef32d781ae
Dries Hooghe <dries@drieshooghe.com>
2020-02-21 14:47:21 +0100

    update name

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 class PageTitle extends Component {
 	render() {
 		return <section className="pb-8">
-			<h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-display text-white text-center leading-snug">Dries Hooghe</h1>
+			<h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-display text-white text-center leading-snug">Johan Hoste</h1>
 		</section>
 	}
 }
