6f71bc7f27669557fa5edcce37bed7982ff78f4a
Dries Hooghe <dries@drieshooghe.com>
2020-02-21 15:47:14 +0100

    add static content

---
+++ content.ts
@@ -0,0 +1,93 @@
+export const Content = {
+	general: {
+		website: {
+			title: 'Johan Hoste | Pianostemmer en restaurateur',
+			description: 'Johan Hoste is een pianostemmer en restaurateur uit regio Kortrijk. Met 38 jaar ervaring komt hij aan huis uw piano onderhouden en stemmen op gehoor.'
+		},
+		contact: {
+			name: 'Johan Hoste',
+			street: 'Meynaertkouter',
+			number: '4',
+			postalCode: '8540',
+			town: 'Deerlijk',
+			phone: '+32 475 69 33 60',
+			email: 'pianostemmer@johanhoste.be',
+			vatNumber: 'BE76 7360 5063 7195',
+			geoLink: 'https://goo.gl/maps/ptRwLYRP3tz'
+		}
+	},
+	pages: {
+		'404': {
+			title: 'Pagina niet gevonden',
+			content: [
+				'De verkeerde noot gespeeld, de door u verzochte pagina lijkt niet te bestaan...',
+				'Keer terug naar de:'
+			],
+			buttonText: 'homepagina',
+			buttonLink: '/'
+		},
+		home: {
+			title: 'Johan Hoste',
+			content: 'Pianostemmer en restaurateur'
+		},
+		about: {
+			title: 'Wie ben ik?',
+			content: [
+				{
+					p: 'Op 1 oktober 2018 ben ik gestart als pianostemmer en restaurateur in bijberoep.'
+				},
+				{
+					p: 'De vakkennis heb ik meegekregen van Pieter Loncke jr (1901 - 2001), lid van de bekende orgelbouwer familie Loncke.'
+				},
+				{
+					p: 'Reeds 38 jaar heb ik ervaring in het stemmen van akoestische piano\'s op het gehoor.'
+				},
+				{
+					p: 'Ik heb piano gestudeerd aan de muziekacademie te Veurne en ben dit altijd blijven koesteren. Uiteraard heb ik de liefde voor de klassieke muziek met de paplepel meegekregen. Mijn vader Albert Hoste was piano leraar, muziekleraar in het college te Veurne en koster-organist in de Sint-Niklaaskerk te Veurne.'
+				}
+			],
+			cta: {
+				message: 'Ik kom uw piano stemmen bij u thuis. U mag mij contacteren via Messenger, mail of het aanvraagformulier van deze website, voor een afspraak indien u dit wenst.',
+				buttonText: 'Stel een vraag',
+				buttonLink: '/contact'
+			}
+		},
+		services: {
+			title: 'Welke diensten bied ik aan?',
+			content: [
+				{
+					t: 'Stemmen van uw piano',
+					p: 'Ik stem zowel buffetpiano\'s als vleugelpiano\'s.\nPer stembeurt is de prijs 80 euro voor een <strong>buffetpiano</strong> en 120 euro voor een <strong>vleugelpiano</strong>.'
+				},
+				{
+					t: 'Kleine herstellingen aan mechaniek en snaren',
+					p: 'Herstellingen aan de mechaniek of vervangen van snaren wordt per uur verrekend aan 50 euro / uur.'
+				},
+				{
+					t: 'Abonnementen',
+					p: 'Ik bied een abonnement aan voor een halfjaarlijkse stembeurt voor uw piano. Kostprijs 120 euro per 2 stembeurten voor een buffetpiano, 180 euro per 2 stembeurten voor een vleugelpiano. herstellingen van de mechaniek worden hier ook als extra\'s aangerekend, alsook de verplaatsingskosten buiten regio Zuid- West - Vlaanderen.'
+				},
+				{
+					t: 'Verplaatsingskosten',
+					p: 'Ik bied mijn diensten aan in alle regio\'s.Voor verplaatsingen buiten Zuid - West - Vlaanderen wordt echter wel een kilometervergoeding aangerekend.'
+				}
+			],
+			cta: {
+				message: 'Heeft u nog vragen?',
+				buttonText: 'Stuur gerust een bericht!',
+				buttonLink: '/contact'
+			}
+		},
+		termsAndConditions: {
+			title: 'Privacybeleid'
+		},
+		testimonials: {
+			title: 'Klanten aan het woord',
+			content: []
+		},
+		contact: {
+			title: 'Vragen? Neem gerust contact op!',
+			content: ''
+		}
+	}
+}
