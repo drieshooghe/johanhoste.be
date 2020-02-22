bc287c2ef47b25c5ba77539d24c19086ce842f2f
Dries Hooghe <dries@drieshooghe.com>
2020-02-22 15:04:45 +0100

    change order of plugins

--- next.config.js
+++ next.config.js
@@ -2,15 +2,9 @@ const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 
-module.exports = withImages(
+module.exports =
 	withCss(
-		withSass({
-			exportTrailingSlash: true,
-			exportPathMap: function () {
-				return {
-					'/': { page: '/' }
-				};
-			}
-		})
+		withSass(
+			withImages()
+		)
 	)
-)
