e96fe37fca6422c136187210bf1930a5242efa0c
Dries Hooghe <dries@drieshooghe.com>
2020-02-22 13:10:21 +0100

    add footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,13 +1,35 @@
 import { Component } from "react";
 import CookieConsent from "react-cookie-consent";
 import { Content } from '../../content';
+import Link from "next/link";
 
 export class Footer extends Component {
 	render() {
 		return <footer className="block trans-slow bg-black py-6">
 			<div className="container w-auto text-xs md:text-sm">
-				{/* <FooterTop></FooterTop>
-				<FooterBottom></FooterBottom> */}
+				<section className="flex flex-wrap md:text-center">
+
+					<div className="py-1 w-full md:w-1/3 trans">
+						<span className="text-white">©&nbsp;{new Date().getFullYear()}&nbsp;{Content.general.contact.name}</span>
+					</div>
+
+					<div className="py-1 w-full md:w-1/3 trans">
+						<Link href="/disclaimer" as="/disclaimer">
+							<a className='link'>
+								<span className="text-white link__label link__label--dark">Disclaimer</span>
+							</a>
+						</Link>
+					</div>
+
+					<div className="py-1 w-full md:w-1/3 trans">
+						<Link href="https://www.drieshooghe.com">
+							<a className='link text-white'>
+								Website by:&nbsp;<span className="text-white link__label link__label--dark">Dries Hooghe</span>
+							</a>
+						</Link>
+					</div>
+
+				</section>
 				<CookieConsent
 					location='bottom'
 					buttonText={Content.general.disclaimer.buttonMessage}
@@ -18,6 +40,6 @@ export class Footer extends Component {
 					</div>
 				</CookieConsent>
 			</div>
-		</footer>
+		</footer >
 	}
 }
