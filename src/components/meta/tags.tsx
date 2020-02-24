e6667d37af2df1ca7ddb7ab9fa90a318b8bb9ab3
Dries Hooghe <dries@drieshooghe.com>
2020-02-24 23:10:35 +0100

    compile

--- src/components/meta/tags.tsx
+++ src/components/meta/tags.tsx
@@ -8,7 +8,6 @@ export class HeaderTags extends Component {
       <Head>
         <title>Johan Hoste</title>
         <link href='https://fonts.googleapis.com/css?family=Oswald:200,300,500&display=swap' rel='stylesheet' />
-        <link rel='stylesheet' href='style/main.css' />
       </Head>
     );
   }
