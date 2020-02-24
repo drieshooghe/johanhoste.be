c844d250a37c73266ad2b324d1656fb5971aa652
Dries Hooghe <dries@drieshooghe.com>
2020-02-24 23:08:37 +0100

    compile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 
-module.exports = withCss(withSass(withImages()));
+module.exports = withSass(withImages());
