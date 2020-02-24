c4a1f76ae6e699d33e81b3ae86e3cd7ebcd8d1d9
Dries Hooghe <dries@drieshooghe.com>
2020-02-24 23:30:05 +0100

    compile for export

---
+++ out/_next/static/webpack/static/development/pages/index.js.d06885c98be5fe203a84.hot-update.js
@@ -0,0 +1,105 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./content/content.ts":
+/*!****************************!*\
+  !*** ./content/content.ts ***!
+  \****************************/
+/*! exports provided: Content */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Content", function() { return Content; });
+var Content = {
+  general: {
+    website: {
+      title: 'Johan Hoste | Pianostemmer en restaurateur',
+      description: 'Johan Hoste is een pianostemmer en restaurateur uit regio Kortrijk. Met 38 jaar ervaring komt hij aan huis uw piano onderhouden en stemmen op gehoor.'
+    },
+    contact: {
+      name: 'Johan Hoste',
+      street: 'Meynaertkouter',
+      number: '4',
+      postalCode: '8540',
+      town: 'Deerlijk',
+      phone: '+32 475 69 33 60',
+      email: 'pianostemmer@johanhoste.be',
+      vatNumber: 'BE76 7360 5063 7195',
+      geoLink: 'https://goo.gl/maps/ptRwLYRP3tz'
+    },
+    disclaimer: {
+      message: 'Deze website maakt gebruik van cookies om uw gebruikerservaring te optimaliseren. Wilt u meer weten over deze cookies?',
+      ctaMessage: 'Klik dan hier.',
+      ctaLink: '/disclaimer',
+      buttonMessage: 'Deze cookies zijn oké 🍪',
+      expires: 150
+    }
+  },
+  pages: {
+    '404': {
+      title: 'Pagina niet gevonden',
+      body: ['De verkeerde noot gespeeld, de door u verzochte pagina lijkt niet te bestaan...', 'Keer terug naar de:'],
+      buttonText: 'homepagina',
+      buttonLink: '/'
+    },
+    home: {
+      title: 'Johan Hoste',
+      body: 'Pianostemmer en restaurateur'
+    },
+    about: {
+      title: 'Wie ben ik?',
+      body: [{
+        p: 'Op 1 oktober 2018 ben ik gestart als pianostemmer en restaurateur in bijberoep.'
+      }, {
+        p: 'De vakkennis heb ik meegekregen van Pieter Loncke jr (1901 - 2001), lid van de bekende orgelbouwer familie Loncke.'
+      }, {
+        p: 'Reeds 38 jaar heb ik ervaring in het stemmen van akoestische piano\'s op het gehoor.'
+      }, {
+        p: 'Ik heb piano gestudeerd aan de muziekacademie te Veurne en ben dit altijd blijven koesteren. Uiteraard heb ik de liefde voor de klassieke muziek met de paplepel meegekregen. Mijn vader Albert Hoste was piano leraar, muziekleraar in het college te Veurne en koster-organist in de Sint-Niklaaskerk te Veurne.'
+      }],
+      cta: {
+        message: 'Ik kom uw piano stemmen bij u thuis. U mag mij contacteren via Messenger, mail of het aanvraagformulier van deze website, voor een afspraak indien u dit wenst.',
+        buttonText: 'Stuur een bericht',
+        buttonLink: '/contact'
+      }
+    },
+    services: {
+      title: 'Welke diensten bied ik aan?',
+      body: [{
+        t: 'Stemmen van uw piano',
+        p: 'Ik stem zowel buffetpiano\'s als vleugelpiano\'s.\nPer stembeurt is de prijs 80 euro voor een <strong>buffetpiano</strong> en 120 euro voor een <strong>vleugelpiano</strong>.'
+      }, {
+        t: 'Kleine herstellingen aan mechaniek en snaren',
+        p: 'Herstellingen aan de mechaniek of vervangen van snaren wordt per uur verrekend aan 50 euro / uur.'
+      }, {
+        t: 'Abonnementen',
+        p: 'Ik bied een abonnement aan voor een halfjaarlijkse stembeurt voor uw piano. Kostprijs 120 euro per 2 stembeurten voor een buffetpiano, 180 euro per 2 stembeurten voor een vleugelpiano. herstellingen van de mechaniek worden hier ook als extra\'s aangerekend, alsook de verplaatsingskosten buiten regio Zuid- West - Vlaanderen.'
+      }, {
+        t: 'Verplaatsingskosten',
+        p: 'Ik bied mijn diensten aan in alle regio\'s.Voor verplaatsingen buiten Zuid - West - Vlaanderen wordt echter wel een kilometervergoeding aangerekend.'
+      }],
+      cta: {
+        message: 'Heeft u nog vragen?',
+        buttonText: 'Stuur een bericht',
+        buttonLink: '/contact'
+      }
+    },
+    termsAndConditions: {
+      title: 'Privacybeleid'
+    },
+    testimonials: {
+      title: 'Klanten aan het woord',
+      body: []
+    },
+    contact: {
+      title: 'Vragen? Neem gerust contact op!',
+      embeddedMapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2518.5967144073966!2d3.370028415746159!3d50.85715187953355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c33d5cdb153c8f%3A0x11945ba27edf0e54!2sJohan%20Hoste!5e0!3m2!1snl!2sbe!4v1582579915729!5m2!1snl!2sbe',
+      embeddedFormLink: ''
+    }
+  }
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.d06885c98be5fe203a84.hot-update.js.map
